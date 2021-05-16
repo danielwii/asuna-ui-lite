2225d161408ff2b76e34f12140f6179adc93d660
mercury.wang <mercwang@hotmail.com>
2021-05-16 21:50:52 +0800

    feat: layout, header

---
+++ src/layout.tsx
@@ -0,0 +1,19 @@
+import React, { CSSProperties } from 'react';
+
+export interface BasicLayoutProps {
+  className?: string; // tailwind css style
+  style?: CSSProperties;
+  content?: React.ReactNode;
+}
+
+const Header: React.FC<BasicLayoutProps> = ({ className, style, children }) => (
+  <div className={`${className} w-full`} style={style}>
+    {children}
+  </div>
+);
+
+const Layout = ({ children }): JSX.Element => <div className="flex flex-col h-screen w-full">{children}</div>;
+
+Layout.Header = Header;
+
+export { Layout };
---
+++ stories/layout.stories.tsx
@@ -0,0 +1,11 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { Layout } from '../src/layout';
+
+const { Header } = Layout;
+
+storiesOf('Layout', module).add('Default', () => (
+  <Layout>
+    <Header className="h-64 bg-blue-200">The header</Header>
+  </Layout>
+));
