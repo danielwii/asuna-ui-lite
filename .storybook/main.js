79cc68c58cefa296d7b9af669d63859684459163
Daniel Wei <danielwii0326@gmail.com>
2021-11-24 14:26:31 +0800

    fix: webpack issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  core: { builder: 'webpack5' },
+  // core: { builder: 'webpack5' },
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     { name: '@storybook/addon-postcss', options: { postcssLoaderOptions: { implementation: require('postcss') } } },
